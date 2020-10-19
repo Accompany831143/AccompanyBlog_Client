c148f2629682ed83a5d8bd88f5205e80e96f145c
Accompany831143 <accompany_zhao@163.com>
2020-10-19 20:47:02 +0800

    添加less

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
+  <div class="aiva_home">
     <h1>Accompany</h1>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.aiva_home {
+  
+}
 </style>
