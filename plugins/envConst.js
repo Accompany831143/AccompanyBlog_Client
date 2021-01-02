2da8c6c22b7de1892ed6f22f08c2b97898cb1aec
Accompany831143 <Accompany_zhao@163.com>
2021-01-02 15:24:23 +0800

    新建测试环境变量

--- plugins/envConst.js
+++ plugins/envConst.js
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2020-12-17 17:51:37
+ * @LastEditors: Aiva
+ * @LastEditTime: 2021-01-02 15:16:46
+ * @FilePath: \AivaBlog_Client\plugins\envConst.js
+ */
 const envObject = {
     development:{
         baseUrl:'http://localhost:7758/api/home/',
@@ -7,6 +13,10 @@ const envObject = {
         baseUrl:'https://aiva.vip/api/home/',
         pathUrl:'https://aiva.vip/'
 
+    },
+    test:{
+        baseUrl:'http://192.168.0.189/api/home/',
+        pathUrl:'http://192.168.0.189/'
     }
 
 }
