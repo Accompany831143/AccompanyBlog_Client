292a1d963810390e16ed3b5a23da3901c7d96006
Accompany831143 <Accompany_zhao@163.com>
2021-01-04 16:55:24 +0800

    更改环境变量地址

--- plugins/envConst.js
+++ plugins/envConst.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-12-17 17:51:37
  * @LastEditors: Aiva
- * @LastEditTime: 2021-01-02 15:16:46
+ * @LastEditTime: 2021-01-04 16:54:24
  * @FilePath: \AivaBlog_Client\plugins\envConst.js
  */
 const envObject = {
@@ -10,13 +10,13 @@ const envObject = {
         pathUrl:'http://localhost:7758/'
     },
     production:{
-        baseUrl:'https://aiva.vip/api/home/',
-        pathUrl:'https://aiva.vip/'
+        baseUrl:'https://blog-server:7758/api/home/',
+        pathUrl:'https://blog-server:7758/'
 
     },
     test:{
-        baseUrl:'http://192.168.0.189/api/home/',
-        pathUrl:'http://192.168.0.189/'
+        baseUrl:'http://blog-server:7758/api/home/',
+        pathUrl:'http://blog-server:7758/'
     }
 
 }
